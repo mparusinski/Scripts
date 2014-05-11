b1d26c9735c7bf6a4eb90828196bd6d1a457d592
Michal Parusinski <mparusinski@gmail.com>
2014-05-11 16:12:33 +0100

    Firewall will now support loopback device

--- firewall.sh
+++ firewall.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+echo "Stopping iptables-persistent service"
+service iptables-persistent flush
+
 echo "Flushing current rules"
 iptables -F
+
+echo "Allowing loopback devices to connect"
+iptables -A INPUT -i lo -j ACCEPT
 
 echo "Allowing related and established connections"
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
@@ -14,3 +20,8 @@ iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 
 echo "Disabling all remaining traffic"
 iptables -A INPUT -j DROP
+
+echo "---------------------------------------------------------"
+echo "WARNING THIS SCRIPT DOES NOT MAKE THE SETTINGS PERSISTENT"
+echo "TEST EVERYTHING WORKS THEN MAKE PERSISTENT"
+echo "---------------------------------------------------------"
