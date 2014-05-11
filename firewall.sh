8d32931fa950f4df953335005ea4410fde3f892d
Michal Parusinski <mparusinski@gmail.com>
2014-05-11 16:02:39 +0100

    Fixing issues with the script

--- firewall.sh
+++ firewall.sh
@@ -4,7 +4,7 @@ echo "Flushing current rules"
 iptables -F
 
 echo "Allowing related and established connections"
-iptables -A INPUT -m conntrack --cstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 echo "Allowing ssh to connect freely"
 iptables -A INPUT -p tcp --dport ssh -j ACCEPT
@@ -12,5 +12,5 @@ iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 echo "Allowing webtraffic as some programs may use a webserver"
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 
-ehco "Disabling all remaining traffic"
+echo "Disabling all remaining traffic"
 iptables -A INPUT -j DROP
